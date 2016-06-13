f13b791416cfca591c26414d8cc0e397596d247f
panicboy <asasaki@gmail.com>
2016-06-13 12:45:27 -1000

    function placeholders added

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,40 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
 
+ var linkedListGenerator = (function(){
+  var theNode = null;
+
+function _getHead() {
+  return theNode;
 }
+
+function _getTail(){
+  if (theNode === null) {
+    return theNode;
+  }
+}
+
+function _add(){
+}
+
+function _get(){
+}
+
+function _remove(){
+}
+
+function _insert(){
+}
+
+
+  return {
+    getHead: _getHead,
+    getTail: _getTail,
+    add: _add,
+    get: _get,
+    remove: _remove,
+    insert: _insert
+  };
+
+});
